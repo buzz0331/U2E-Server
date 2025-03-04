49317d14fc637872bdbea9ba972f302da2bd99d2
Jay Park <pjh6183@naver.com>
2025-03-04 17:37:51 +0900

    first commit

---
+++ U2E/src/main/java/Konkuk/U2E/U2EApplication.java
@@ -0,0 +1,13 @@
+package Konkuk.U2E;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2EApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2EApplication.class, args);
+	}
+
+}
---
+++ U2E/src/test/java/Konkuk/U2E/U2EApplicationTests.java
@@ -0,0 +1,13 @@
+package Konkuk.U2E;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2EApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
